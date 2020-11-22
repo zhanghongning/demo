57fd5686b8d4301ac5990ad6524d694be8d268e7
zhanghongning <115779425@qq.com>
2020-11-22 12:32:34 +0800

    新加内容

--- main.cpp
+++ main.cpp
@@ -1,8 +1,12 @@
-#include<iostrame>
+# include <iostrame>
 using namespace std;
 int main(){
 
 cout<<"hello world!"<<endl ;
-cout << "ning /n";
+cout << "zhang hong: nihao "<<endl;
+int a ,b;
+a=10;
+
+return  0
 
 }
